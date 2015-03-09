8416abed7193f064b9aed700da069fd3adc4c597
Stine Hansen <stinehansen@Stines-MacBook-Pro.local>
2015-03-09 13:50:14 -0700

    compact it

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,10 @@
 ('#sky').jQlouds({
 minClouds: 5, 
 maxClouds: 10,
+maxWidth: 300,
+minWidth: 200,
+wind: true
+src: '../img/new-cloud.png',
+
 });
 
-
-$('#sky').jQlouds({
-  maxWidth: 300,
-  minWidth: 200,
-});
-
-$('#sky').jQlouds({
-  wind: true
-});
-
-$('#sky').jQlouds({
-  src: '../img/new-cloud.png',
-});
