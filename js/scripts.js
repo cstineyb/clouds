b92d4225bf2994c06487ca274c807d3635242a22
Stine Hansen <stinehansen@Stines-MacBook-Pro.local>
2015-03-09 14:03:39 -0700

    fixed it

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 
 
-('#sky').jQlouds({
-minClouds: 5, 
-maxClouds: 10,
-maxWidth: 300,
-minWidth: 200,
-wind: true
-src: '../img/new-cloud.png',
+$('body').jQlouds({
+	minClouds: 5, 
+	maxClouds: 10,
+	maxWidth: 300,
+	minWidth: 200,
+	wind: true,
+	src: 'img/new-cloud.png',
 
 });
 
